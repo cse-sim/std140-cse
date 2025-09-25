8a1991a5462d90704f12490e79bdca283786dd1d
Nathan Oliver <nathan.oliver@bigladdersoftware.com>
2025-09-25 09:50:38 -0600

    Update CSE version and release date in results report.

--- scripts/std-140/write-results.py
+++ scripts/std-140/write-results.py
@@ -132,8 +132,8 @@ for case in cases:
 
     information_sheet = template["Information"]
     information_sheet.cell(row=2, column=2, value=case)
-    information_sheet.cell(row=3, column=2, value="CSE 0.925.0")
-    information_sheet.cell(row=4, column=2, value="Dec 18, 2024")
+    information_sheet.cell(row=3, column=2, value="CSE 0.926.0+transmitted-solar.99c6f0b3.21")
+    information_sheet.cell(row=4, column=2, value="Aug 31, 2025")
     information_sheet.cell(row=5, column=2, value="Big Ladder Software")
     information_sheet.cell(
         row=6,
